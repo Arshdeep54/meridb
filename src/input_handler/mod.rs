3e02fb80c3f013fb1848c48c5596f4ac8164b86a
Arshdeep54 <balarsh535@gmail.com>
2025-12-21 22:40:44 +0530

    Remove tokenkind and old src folder
    Signed-off-by: Arshdeep54 <balarsh535@gmail.com>

--- src/input_handler/mod.rs
+++
@@ -1,8 +0,0 @@
-mod editor;
-mod history;
-mod terminal;
-
-pub use editor::Editor;
-pub use history::History;
-
-pub use editor::InputHandler;
--- src/storage/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod database;
-pub mod page;
-pub mod record;
-pub mod table;
-pub mod types;
-
-pub use page::Page;
-pub use record::Record;
-pub use table::Table;
